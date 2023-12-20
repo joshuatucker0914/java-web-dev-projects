6a6241da750befdce6f1e7214b00f64d54b97657
joshuatucker0914 <joshuatucker0914@gmail.com>
2023-12-20 13:05:21 -0600

    complete

--- unit-testing/studio/balanced-brackets/src/test/java/org/launchcode/BalancedBracketsTest.java
+++ unit-testing/studio/balanced-brackets/src/test/java/org/launchcode/BalancedBracketsTest.java
@@ -10,4 +10,78 @@ class BalancedBracketsTest {
     public void emptyTest() {
         assertEquals(true, true);
     }
+
+    @Test
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+
+    @Test
+    public void testBalancedBrackets() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[LaunchCode]"));
+        assertTrue(BalancedBrackets.hasBalancedBrackets("Launch[Code]"));
+    }
+
+    @Test
+    public void testUnbalancedBrackets() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[LaunchCode"));
+        assertFalse(BalancedBrackets.hasBalancedBrackets("Launch]Code["));
+        assertFalse(BalancedBrackets.hasBalancedBrackets("["));
+    }
+
+    @Test
+    public void testNestedBrackets() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[Launch[Code]]"));
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[Launch[Code]"));
+    }
+
+    @Test
+    public void testMixedCharacters() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("Hello[World]!"));
+        assertFalse(BalancedBrackets.hasBalancedBrackets("Launch[Code] is awesome]"));
+    }
+
+    @Test
+    public void testMultipleBracketPairs() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[{()}]"));
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[{()]"));
+    }
+
+    @Test
+    public void testNonBracketCharacters() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("Hello World!"));
+        assertTrue(BalancedBrackets.hasBalancedBrackets("Launch Code is [awesome]!"));
+    }
+
+    @Test
+    public void testEdgeCases() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+        assertFalse(BalancedBrackets.hasBalancedBrackets("["));
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]"));
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[]["));
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[]]"));
+    }
+
+    @Test
+    public void testEdgeCaseWithNonBracketCharacters() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("Hello [World]! This is [a test]"));
+    }
+
+    @Test
+    public void testEdgeCaseWithMixedBracketTypes() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[{Hello}]"));
+    }
+
+    @Test
+    public void testEdgeCaseWithMixedBracketOrder() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]["));
+    }
+
+    @Test
+    public void testEdgeCaseWithMixedCharactersAndBrackets() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("Launch[Code] is awesome]!"));
+    }
+
+
 }
