d05e6e29cbf8b42d8907a5750b883dc74715e13e
joshuatucker0914 <joshuatucker0914@gmail.com>
2024-01-28 07:37:03 -0600

    recoded

--- hello-spring/src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ hello-spring/src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -1,43 +1,51 @@
-//package org.launchcode.hellospring.controllers;
-//
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.*;
-//
-//@Controller
-//@ResponseBody
-//@RequestMapping("hello")
-//public class HelloController {
-//    //Handles requests at paths "/hello"
-//
-////    @GetMapping("hello")
-////    @ResponseBody
-////    public String hello(){
-////        return "Hello, Spring!";
-////    }
-//
-//    @GetMapping("bye")
-//    public String bye(){
-//        return "Goodbye, Spring!";
+package org.launchcode.hellospring.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class HelloController {
+    //Handles requests at paths "/hello"
+
+//    @GetMapping("hello")
+//    @ResponseBody
+//    public String hello(){
+//        return "Hello, Spring!";
 //    }
-//
-//    //Handles requests of the form /hello?name=LaunchCode
-//    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST},value = "hello")
-//    public String helloWithParam(@RequestParam String name){
-//        return "Hello, " + name;
-//    }
-//
-//    @GetMapping("form")
-//    public String helloForm (){
-//        return "<html>" +
-//                "<body>" +
-//                "<form action = 'hello' method = 'post' >" +
-//                "<input type = 'text' name = 'name' >" +
-//                "<input type = 'submit' value = 'Greet me!' >" +
-//                "</form>" +
-//                "</body>" +
-//                "</html>";
-//    }
-//
-//
-//}
-//
+
+    @GetMapping("bye")
+    @ResponseBody
+    public String bye(){
+        return "Goodbye, Spring!";
+    }
+
+    //Handles requests of the form /hello?name=LaunchCode
+    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST},value = "hello")
+    public String helloWithParam(@RequestParam String name, Model model){
+        String greeting = "Hello, " + name + "!";
+        model.addAttribute("greeting",greeting);
+        return "hello";
+    }
+
+    @GetMapping("form")
+    public String helloForm (){
+        return "FormExercise";
+    }
+
+    @GetMapping("hello-names")
+    public String helloNames(Model model){
+        List<String> names = new ArrayList<>();
+        names.add("Java");
+        names.add("LaunchCode");
+        names.add("JavaScript");
+        model.addAttribute("names", names);
+        return "hello-list";
+    }
+
+
+}
+
